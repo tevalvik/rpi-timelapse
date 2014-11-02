5d6bf0f9db3aa93c9e7007e2a528e224b6845711
Trond Eivind Valvik <sneglen1+github@gmail.com>
2014-11-02 21:53:50 +0000

    Updated test.py

--- test.py
+++ test.py
@@ -18,10 +18,18 @@ FRAMES = FRAMES_PER_HOUR * 24 * VIDEO_DAYS
 def time_check():
 	current_time = datetime.datetime.now().time()
 	start = datetime.time(16)
-	end = datetime.time(16,27)
+	end = datetime.time(21,59)
 	return start <= current_time <= end
 
-while time_check():
-	print "time check works"
-	time.sleep(5)
 	
+def capture_photo():
+	time_now = datetime.datetime.now().time()
+	with picamera.PiCamera() as camera:
+		time.sleep(2)
+		camera.capture('/home/pi/timelapse/dtu/frame%s.jpg' % time_now)
+
+if time_check():
+	capture_photo()
+else:
+    print "hello"
+
