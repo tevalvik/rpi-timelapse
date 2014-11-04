e658cedff0ed1832d8134991255880dd20d90a8a
= <sneglen1+github@gmail.com>
2014-11-04 14:30:22 +0000

    Removed some unneeded lines of code

--- test.py
+++ test.py
@@ -6,15 +6,6 @@ VIDEO_DAYS = 5
 FRAMES_PER_HOUR = 1
 FRAMES = FRAMES_PER_HOUR * 24 * VIDEO_DAYS
 
-#date = datetime.datetime.now().time() # gives current time without date
-#print (datetime.datetime.now().time())
-#
-#start = datetime.time(16, 15) 
-#end = datetime.time(16,17)
-#print start
-#print end
-#print (start <= date <= end)
-
 def time_check():
 	current_time = datetime.datetime.now().time()
 	start = datetime.time(16)
