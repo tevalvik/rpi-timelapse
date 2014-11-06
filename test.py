3ee2d91beec220cc174822b5ac08085d4b9bb3c3
= <sneglen1+github@gmail.com>
2014-11-06 22:39:23 +0000

    Updated testcode

--- test.py
+++ test.py
@@ -2,22 +2,21 @@ import time
 import datetime
 import picamera
 
-VIDEO_DAYS = 5
-FRAMES_PER_HOUR = 1
-FRAMES = FRAMES_PER_HOUR * 24 * VIDEO_DAYS
-
 def time_check():
+	""" Checks whether the time is within a range """
 	current_time = datetime.datetime.now().time()
 	start = datetime.time(07)
 	end = datetime.time(18)
 	return start <= current_time <= end
-
 	
 def capture_photo():
+	""" Takes a photo and stores it with the time taken as the name"""
 	time_now = datetime.datetime.now().time()
 	with picamera.PiCamera() as camera:
 		time.sleep(2)
 		camera.capture('/home/pi/timelapse/dtu/frame%s.jpg' % time_now)
+
+# Loop taking pictures within a given time.
 while True:
 	if time_check():
 		capture_photo()
--- test.py~
+++ test.py~
@@ -8,8 +8,8 @@ FRAMES = FRAMES_PER_HOUR * 24 * VIDEO_DAYS
 
 def time_check():
 	current_time = datetime.datetime.now().time()
-	start = datetime.time(16)
-	end = datetime.time(21,59)
+	start = datetime.time(07)
+	end = datetime.time(18)
 	return start <= current_time <= end
 
 	
@@ -18,9 +18,9 @@ def capture_photo():
 	with picamera.PiCamera() as camera:
 		time.sleep(2)
 		camera.capture('/home/pi/timelapse/dtu/frame%s.jpg' % time_now)
-
-if time_check():
-	capture_photo()
-else:
-    print "hello"
+while True:
+	if time_check():
+		capture_photo()
+	else:
+		time.sleep(3600)
 
