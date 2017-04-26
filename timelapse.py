58522d0ee869c0aa78b705cf36b039cf2c232e84
tevalvik <sneglen1+github@gmail.com>
2017-04-26 19:31:50 +0200

    Put PATH in a variable.

--- timelapse.py
+++ timelapse.py
@@ -2,8 +2,10 @@ import time
 import datetime
 import picamera
 
+PATH = '/home/pi/timelapse/images/'
+
 def time_check():
-	""" Checks whether the time is within a given timeframe. """
+	''' Checks whether the time is within a given timeframe. '''
 	"Time on the pi is one hour off. 06 = 07"
 	current_time = datetime.datetime.now().time()
 	start = datetime.time(6)
@@ -11,12 +13,13 @@ def time_check():
 	return start <= current_time <= end
 	
 def capture_photo():
-	""" Takes a photo and stores it with the time taken as the name."""
+	''' Takes a photo and stores it with the time taken as the name.'''
 	time_now = datetime.datetime.now()
 	with picamera.PiCamera() as camera:
 		time.sleep(30)
 		print "Taking photo"
-		camera.capture('/home/pi/timelapse/dtu/images/im%s.jpg' % time_now)
+                camera.capture(PATH + "im%s.jpg" % time_now)
+		# camera.capture('/home/pi/timelapse/images/im%s.jpg' % time_now)
 		print "Photo taken at %s " % time_now
 
 # Taking pictures in the given timeframe.
